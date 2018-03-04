b503af26aa36fd67884e5850fcc6051571d0eed9
alfianghz <36778850+alfianghz@users.noreply.github.com>
2018-03-04 20:30:43 +0700

    Update donate-level.hpp

--- xmrstak/donate-level.hpp
+++ xmrstak/donate-level.hpp
@@ -3,7 +3,7 @@
 /*
  * Dev donation.
  * Percentage of your hashing power that you want to donate to the developer, can be 0.0 if you don't want to do that.
- * Example of how it works for the default setting of 1.0:
+ * Example of how it works for the default setting of 2.0:
  * You miner will mine into your usual pool for 98 minutes, then switch to the developer's pool for 2.0 minute.
  * Switching is instant, and only happens after a successful connection, so you never loose any hashes.
  *
@@ -15,4 +15,4 @@
  *
  */
 
-constexpr double fDevDonationLevel = 1.0 / 100.0;
+constexpr double fDevDonationLevel = 2.0 / 100.0;
